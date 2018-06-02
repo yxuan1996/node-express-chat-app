27dd5b68c977b9b54ea699a6c5bf17401bfb43a7
EC2 Default User <ec2-user@ip-172-31-9-62.ec2.internal>
2018-06-02 23:03:17 +0000

    add mongo database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -11,6 +12,17 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+// set the mongo database URI
+// to get the URI, run the following line on the command line
+// heroku config | grep MONGODB_URI
+var dbURL = 'mongodb://heroku_pssj50t6:5r4rn9ujham1q67s0d716gp8gq@ds245240.mlab.com:45240/heroku_pssj50t6'
+
+//Create a message model schema for the database
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 var messages = [
     {name: 'Tim', message: 'Hi'},
@@ -22,13 +34,32 @@ app.get('/messages', (req, res) =>{
 })
 
 app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    //Initialize a database entry
+    var message = new Message(req.body)
+    
+    //save the message as an entry
+    message.save((err) => {
+        if(err)
+            res.sendStatus(500)
+        else
+        messages.push(req.body)
+        io.emit('message', req.body)
+        res.sendStatus(200) 
+        
+    })
+    
+    
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+//connect to the mongo database using the URI stated above
+//2nd argument is to prevent depreciation warning
+//3rd argument is a callback function that logs the connection status and logs any error messages, if any
+mongoose.connect(dbURL, {UseMongoClient: true}, (err) => {
+    console.log('mongo client connected', err)
 })
 
 //We use port 8080 because of cloud9. Normally, the localhost port is 3000. 
